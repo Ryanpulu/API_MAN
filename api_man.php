fd15f09aa0f088841f27c213eb2737231db4bc5f
ryanpulu <ryanpulu@outlook.com>
2017-06-09 23:09:17 +0800

    ss
    ss

---
+++ api_man.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ryan
+ * Date: 2017/6/9
+ * Time: 22:52
+ */
+namespace API_MAN{
+    class API_MAN{
+
+    }
+}
---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ryan
+ * Date: 2017/6/9
+ * Time: 23:08
+ */
+
+use API_MAN;
+$sn = new API_MAN\API_MAN();
